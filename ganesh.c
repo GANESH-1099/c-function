7cbe44213431467d7e47113b7dfe8696f4d4c428
GANESH-1099 <ganeshv1099@gmail.com>
2025-10-04 10:49:52 +0530

    upload

---
+++ ganesh.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+int a=10, b=20;
+void add (int , int);
+void main()
+{ 
+    printf(" hello this is ganesh");
+    add ( a, b );
+}
+void add ( int a, int b )
+{
+ int c=a+b;  
+ printf(" sum the number a and b %d",c);
+
+} 
